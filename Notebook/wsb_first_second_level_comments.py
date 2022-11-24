42369e52d5286b9a2bdf737a75c6243cd118a2e5
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-11-23 16:29:52 -0800

    wsb first and second level comments

--- Notebook/wsb_first_second_level_comments.py
+++ Notebook/wsb_first_second_level_comments.py
@@ -2,45 +2,63 @@ import praw
 from datetime import datetime
 from sys import exit 
 
-
-reddit_read_only = praw.Reddit(client_id=client_id
+reddit_read_only = praw.Reddit(client_id=client_id,
                                client_secret=client_secret,
                                user_agent=user_agent)
 
 subreddit = reddit_read_only.subreddit('wallstreetbets')
-print(type(subreddit))
-exit()
-
 print(subreddit.display_name)
 print('--------------------------------------------------')
 
+# get the first 5 submissions
 submissions = subreddit.search('Discussion')
+
+sub_dic = {}
+count = 1
 for submission in submissions:
-    submission = submission
-    break
-print(submission.title)
-
-top_comments = submission.comments
-print(type(top_comments))
-count = 0
-for tc in top_comments:
-    tc = tc
-    count += 1
-    if count ==2:
+    sub_dic[f'sub_{count}'] = submission
+    if count == 5:
         break
-print(f'top comment:\n{tc.body}')
-print(f'author:\n{tc.author}\n')
+    count += 1
 
-count = 0
+# get the first 5 top comments of the first submission
+top_comments = sub_dic[f'sub_1'].comments
+
+tc_dic = {}
+count = 1
+for tc in top_comments:
+    print(f'progress tc: {count} / {5}')
+    tc_dic[f'tc_{count}'] = tc
+    if count == 5:
+        break
+    count += 1
+
+# get all first level replies to the top comments
 replies_tc = tc.replies
-replies_tc.replace_more(limit=None)
-for reply in replies_tc:
-    print(reply.body)
-    print(reply.author)
-    print('')
+
+reply_dic = {}
+count = 1
+total = len(tc_dic.keys())
+for name, tc in tc_dic.items():
+    print(f'progress replies: {count} / {total}')
+    replies_tc = tc.replies
+    replies_tc.replace_more(limit=None)
+
+    tc_no = name.split('_')[-1]
+    reply_dic[f'tc_{tc_no}_replies'] = []
+    for reply in replies_tc:
+        reply_dic[f'tc_{tc_no}_replies'].append(reply.body)
+    count += 1
 
 
-
-
+for i in range(1, 6):
+    tc = tc_dic[f'tc_{i}']
+    replies = reply_dic[f'tc_{i}_replies']
+    print(f'----------Comment-{i}----------:\n')
+    print(f'{tc.body}\n')
+    print(f'-----Replies-to-comment-{i}-----:\n')
+    for k, r in enumerate(replies):
+        print(f'---Reply-{k+1}---:\n')
+        print(f'{r}\n')
 
 
