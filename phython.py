78dad11d01d66713c886e207a3a10f1236a42929
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-11-27 12:48:12 -0800

    cleaning up phython.py

--- phython.py
+++ phython.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-import yfinance as yf
 import praw
 
 # turn off scientific notation
@@ -9,134 +7,66 @@ np.set_printoptions(suppress=True)
 
 
 # need to remove the gmt offset that comes with yfinance date/time outputs
-class Volitility:
+class stats:
 
-    def __init__(self, ticker=None, data=None, data_path=None):
-        self.ticker = ticker
-        self.data_path = data_path
-        self.data = data
-
-    def historical(self,
+    def historical(stock_price,
                    method=None,
-                   s_date=0,
-                   e_date=-1,
-                   period=None,
-                   interval=None):
+                   s_ind=0,
+                   e_ind=-1):
 
-        data = self.data
-        data_path = self.data_path
-        ticker = self.ticker
+        stock_price = stock_price[s_ind: e_ind]
 
-        if isinstance(data_path, str):
-            data = pd.read_csv(data_path)
+        dt = np.diff(stock_price)
+        # mean_return = np.mean(dt)
+        volitility = np.std(dt)
 
-        elif isinstance(data, pd.DataFrame):
-            data = self.data
-
-        else:
-            data = yf.Ticker(ticker).history(period=period,
-                                             interval=interval,
-                                             # prepost=prepost,
-                                             actions=False)
-
-        if isinstance(s_date, str):
-            s_date = data.index[data['Date'] == s_date].values[0]
-        if isinstance(e_date, str):
-            e_date = data.index[data['Date'] == e_date].values[0]
-
-        data = data.iloc[s_date: e_date]
-
-        if method == 'open':
-            open_p = data['Open'].values
-            self.dt = np.diff(open_p)
-            self.mean_return = np.mean(self.dt)
-            self.volitility = np.std(self.dt)
-
-        elif method == 'close':
-            close_p = data['Close'].values
-            self.dt = np.diff(close_p)
-            self.mean_return = np.mean(self.dt)
-            self.volitility = np.std(self.dt)
-
-        elif method == 'hl':
-            high_p = data['High'].values
-            low_p = data['Low'].values
-            self.dt = high_p - low_p
-            self.mean_return = np.mean(self.dt)
-            self.volitility = np.std(self.dt)
-
-        return np.std(self.dt)
+        return volitility
 
     def implied(self):
 
         # find a way to get options data and black-scholes formula
         return None
 
+    def pearson_corr(x, y):
 
-def pearson_corr(x, y):
-    ux, uy = np.mean(x), np.mean(y)
-    x_cen, y_cen = x - ux, y - uy
+        ux, uy = np.mean(x), np.mean(y)
+        x_cen, y_cen = x - ux, y - uy
 
-    pear_corr = (np.dot(x_cen, y_cen) /
-                 (np.sqrt(np.sum(np.square(x_cen))) *
-                  np.sqrt(np.sum(np.square(y_cen)))))
+        pear_corr = (np.dot(x_cen, y_cen) /
+                     (np.sqrt(np.sum(np.square(x_cen))) *
+                      np.sqrt(np.sum(np.square(y_cen)))))
 
-    return pear_corr
+        return pear_corr
 
+    def norm_dot(x, y):
 
-def norm_dot(x, y):
+        n_dot = (np.dot(x, y) *
+                 (np.sqrt(np.sum(np.square(x))) *
+                  np.sqrt(np.sum(np.square(y)))))
 
-    n_dot = (np.dot(x, y) *
-             (np.sqrt(np.sum(np.square(x))) *
-              np.sqrt(np.sum(np.square(y)))))
+        return n_dot
 
-    return n_dot
-
-
-class Correlation:
-
-    def __init__(self, ticker=None, data_path=None):
-        self.ticker = ticker
-        self.data_path = data_path
-
-    def find_corr(self,
-                  period=None,
-                  interval=None,
+    def find_corr(stock_price,
                   pat_start=None,
                   pat_end=None,
-                  OHLC='Open',
-                  prepost=False,
                   standardize=True):
 
-        data_path = self.data_path
-        ticker = self.ticker
-
-        if isinstance(data_path, str):
-            data = pd.read_csv(data_path)
-
-        else:
-            data = yf.Ticker(ticker).history(period=period,
-                                             interval=interval,
-                                             prepost=prepost,
-                                             actions=False)
-
-        data = data[OHLC].values.reshape((-1, 1))
+        stock_price = stock_price.reshape((-1, 1))
 
         if standardize:
             Mm = MinMaxScaler(feature_range=(0, 1))
-            data_scaled = Mm.fit_transform(data)
+            stock_price = Mm.fit_transform(stock_price)
 
         if isinstance(pat_start, int) and isinstance(pat_end, int):
-            pat = data[pat_start: pat_end]
-            pat_s = data_scaled[pat_start: pat_end]
+            pat = stock_price[pat_start: pat_end]
             pat_len = len(pat)
             pat_ind = [pat_start, pat_end]
 
         corr_scores = []
         for i in range(0, pat_ind[0] + 1):
-            ref_s = data_scaled[i: i + pat_len]
-            p_score_s = pearson_corr(pat_s.reshape(-1), ref_s.reshape(-1))
-            corr_scores.append([i, p_score_s])
+            ref = stock_price[i: i + pat_len]
+            p_score = stats.pearson_corr(pat.reshape(-1), ref.reshape(-1))
+            corr_scores.append([i, p_score])
         corr_scores = np.array(corr_scores)
         corr_scores = corr_scores[corr_scores[:, 1].argsort()][::-1]
 
@@ -161,27 +91,12 @@ class Correlation:
             if len(top_scores) == 9:
                 break
         top_scores = np.array(top_scores)
-        self.top_scores = top_scores
+        top_scores = top_scores
 
         return top_scores
 
 
 class reddit_phy:
-
-    # def __init__(self,
-    #              client_id=None,
-    #              client_secret=None,
-    #              user_agent=None,
-    #              subreddit_name=None):
-
-    #     self.client_id = client_id
-    #     self.client_secret = client_secret
-    #     self.user_agent = user_agent
-    #     self.subreddit_name = subreddit_name
-    #     self.subreddit = praw.Reddit(client_id=self.client_id,
-    #                                  client_secret=self.client_secret,
-    #                                  user_agent=self.user_agent).subreddit(
-    #                                      self.subreddit_name)
 
     # type(subreddit) = praw.models.reddit.subreddit.Subreddit
     # sort_by: 'hot', 'new', 'top', 'rising'
@@ -214,8 +129,6 @@ class reddit_phy:
                 break
             count += 1
 
-        # self.submission_list = submission_list
-
         return submission_list
 
     def comment_getter(submission_list=None,
@@ -228,8 +141,6 @@ class reddit_phy:
         for submission in submission_coms.keys():
             print(submission.title)
             submission_coms[submission] = submission.comments[: no_of_comments]
-
-        # self.submission_comments = submission_comments
 
         return submission_coms
 
@@ -255,14 +166,12 @@ class reddit_phy:
 
             submissions_comments_replies[sub] = comments_replies
 
-        # self.submissions_comments_replies = submissions_comments_replies
-
         return submissions_comments_replies
 
 
 if __name__ == '__main__':
 
-    client_id = 'JkwBK3M4E5CfK11a8oKkcw'                                              
+    client_id = 'JkwBK3M4E5CfK11a8oKkcw'
     client_secret = 'llrNKLscK7Wcu8Fg28uoNsw5-I2LMw'
     user_agent = 'psssat'
 
