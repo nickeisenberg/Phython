46b72e230f250a2c71d9005b5069c730d4f32a42
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2023-01-15 10:50:47 -0800

    dec weekly log return distribution

--- Notebook/black_scholes/distribution_amzn_log_returns.py
+++ Notebook/black_scholes/distribution_amzn_log_returns.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from scipy.stats import norm
 from scipy.optimize import minimize
 import datetime as dt
@@ -37,32 +38,59 @@ _ = fig.add_trace(go.Scatter(x=pdf_support,
                              name=f'Normal pdf',
                              line={'width': 5}))
 _ = fig.update_layout(
-        title={'text': 'log returns of AMZN December 2022'}
+        title={'text': f'log returns of AMZN:\
+                <br>{time[0]} - {time[-1]}'}
         )
 fig.show()
 
 start = amzn_dec.index.values[0]
-
 start_dt = dt.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
 end_dt = start_dt + dt.timedelta(days=5, hours=16)
-
 end = end_dt.strftime('%Y-%m-%d %H:%M:%S')
 
-amzn_week1 = amzn_dec.loc[start: end]
+t_format = '%Y-%m-%d %H:%M:%S'
+week_data = {}
+week_dates = {}
+for i in range(4):
+    w_start_dt = start_dt + dt.timedelta(days=7*i)
+    w_end_dt = w_start_dt + dt.timedelta(days=5, hours=16)
+    w_start_str = dt.datetime.strftime(w_start_dt, t_format)
+    w_end_str = dt.datetime.strftime(w_end_dt, t_format)
+    week_dates[i] = [w_start_str, w_end_str]
+    week_data[i] = amzn_dec.loc[w_start_str: w_end_str]
 
-week1_open = amzn_week1['Open'].values
+week_log_returns = {}
+for i in range(4):
+    week_log_returns[i] = np.diff(np.log(week_data[i]['Open'].values))
 
-week1_log_returns = np.diff(np.log(week1_open))
+def sub_plot_indicators(nrows, ncols):
+    sub_plot_no = []
+    for i in range(1, nrows * ncols + 1):
+        sub_plot_no.append([int(np.floor(1 + ((i - 1) / ncols))),
+                            int(i - ncols * np.floor((i - 1) / ncols))])
+    return sub_plot_no
 
-mu_week1 = week1_log_returns.mean()
-std_week1 = week1_log_returns.std()
-
-norm_rv_week1 = norm(mu_week1, std_week1)
-
-fig = go.Figure()
-_ = fig.add_trace(go.Histogram(x=week1_log_returns, nbinsx=1000))
-_ = fig.add_trace(go.Scatter(x=pdf_support, y=norm_rv_week1.pdf(pdf_support)))
-_ = fig.update_layout(
-        title={'text': 'week one of December log returns'}
-        )
+sub_p_titles = []
+for i in range(4):
+    sub_p_titles.append(f'{week_dates[i][0]} --- {week_dates[i][1]}')
+fig = make_subplots(rows=2, cols=2,
+                    subplot_titles=sub_p_titles)
+sub_p_nos = sub_plot_indicators(2, 2)
+for i in range(4):
+    data = week_log_returns[i]
+    mu, std = data.mean(), data.std()
+    print(mu, std)
+    rv = norm(mu, std)
+    pdf = rv.pdf
+    support = np.linspace(rv.ppf(.0001), rv.ppf(.9999), 1000)
+    _ = fig.add_trace(
+            go.Histogram(x=data, nbinsx=100, showlegend=False),
+            row=sub_p_nos[i][0], col=sub_p_nos[i][1])
+    _ = fig.add_trace(
+            go.Scatter(x=support, y=pdf(support), showlegend=False,
+                       line={'width': 4,
+                             'color': 'black'}),
+            row=sub_p_nos[i][0], col=sub_p_nos[i][1])
+fig.update_layout(
+        title={'text': f'Amzn weekly log returns'})
 fig.show()
